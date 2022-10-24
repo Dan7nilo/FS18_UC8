27bc397d8f2be2c290b814901bedf66f2b1447fb
Dan7nilo <dan6nilo@gmail.com>
2022-10-24 20:15:48 -0300

    Update:Atividades-4&5

--- UC8=Encontro-Remoto-2(Ultilizando-While).js
+++ UC8=Encontro-Remoto-2(Ultilizando-While).js
@@ -1,19 +1,16 @@
-// Encontro remoto 2
+// Atividade 5: Encontro Remoto 2:
 
-// OK Percorrer de Zero até o número de alunos
+// Listar alunos de 1 a 10
 
-// OK Se o número for par, escreva ‘par’ e o número correspondente
-
-// OK Se o número for ímpar, escreva ‘ímpar’ e o número correspondente 
-
-// OK Se o número for zero, escreva ‘zero’ e o número correspondente
+//Percorrer de Zero até o número de alunos
+//Se o número for par, escreva ‘par’ e o número correspondente
+//Se o número for ímpar, escreva ‘ímpar’ e o número correspondente 
+//Se o número for zero, escreva ‘zero’ e o número correspondente
 
 let numeroDeAlunos = 10
 
 let contador = 0
 while (contador <= numeroDeAlunos){
-    //console.log (contador)
-
      if(contador == 0){
         console.log("o numero atual eh zero")
      }else if (contador % 2 != 1){
@@ -23,3 +20,31 @@ while (contador <= numeroDeAlunos){
      }
      contador++
 }
+
+//////////////////////////////////////////
+
+// Atividade 4: Verificar o nome dos alunos individualmente
+
+let nomeDeAlunos = ["Danilo","Caique","Luana","Rose"]
+for (let nome of nomeDeAlunos){
+    console.log(`Esta pessoa se chama ${nome}`)
+}
+
+// Verificando se a pessoa tem permisão para entrar na festa
+// Requisitos:
+// Ter mais de 18 anos;
+// Ter um ingresso pra festa;
+
+// Idade da pessoa:
+ let idade = 22
+ // Essa pessoa tem ingresso?
+ let ingresso = true 
+ // "=" é ultilizado para atribuir um valor
+ if (idade >= 18 && ingresso == true 
+    //"==" é ultilizado para comparar um valor
+    ){
+    console.log("bem vindo a festa!")
+
+ }else{
+    console.log("Voce não tem permissão para entrar")
+ }
